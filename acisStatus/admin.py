f517d6530ef50054620be5e51444c48010b5e9c5
David Leacock <dave.leacock@gmail.com>
2016-02-12 11:30:42 -0600

    acisStatus app first commit

---
+++ acisStatus/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Beamline
+
+admin.site.register(Beamline)
---
+++ acisStatus/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+class Beamline(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+	notes = models.CharField(max_length=250, blank=True)
+	locked = models.BooleanField(default=False)
+	updated_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.updated_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.name
