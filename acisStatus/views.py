ebe0bbf9ae49697de00a4e576e1ca152c0749a6c
David Leacock <dave.leacock@gmail.com>
2016-02-12 13:41:00 -0600

    Further view fix

--- acisStatus/views.py
+++ acisStatus/views.py
@@ -1,11 +1,11 @@
-from django.templates import RequestContext
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 from .models import Beamline
 
 
 def beamline_status(request):
-	context = RequestContext(request):
+	context = RequestContext(request)
 	beamline_list = Beamline.objects.order_by('name')
-	context_dict = {'beamlines:' beamline_list}
+	context_dict = {'beamlines': beamline_list}
 	return render_to_response(request, 'acisStatus/status.html', 
 		context_dict, context)
