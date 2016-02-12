6df3059a827792943c622df996c01d26cc1342b0
David Leacock <dave.leacock@gmail.com>
2016-02-12 14:12:19 -0600

    Switched to different render method in view

--- acisStatus/views.py
+++ acisStatus/views.py
@@ -1,11 +1,9 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render
+from django.utils import timezone
 from .models import Beamline
 
 
 def beamline_status(request):
-	context = RequestContext(request)
-	beamline_list = Beamline.objects.order_by('name')
-	context_dict = {'beamlines': beamline_list}
-	return render_to_response(request, 'acisStatus/status.html', 
-		context_dict, context)
+	beamlines = Beamline.objects.filter(updated_date__lte=timezone.now()).order_by('updated_date')
+	return render(request, 'acisStatus/status.html', 
+		{'beamlines': beamlines})
