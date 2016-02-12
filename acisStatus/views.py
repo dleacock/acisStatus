24422f4bde0c2aaec64a60dc838f1a3184e7db4c
David Leacock <dave.leacock@gmail.com>
2016-02-12 13:35:29 -0600

    Added templating to view and html file

--- acisStatus/views.py
+++ acisStatus/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from .models import Beamline
+
 
 def beamline_status(request):
-	return render(request, 'acisStatus/status.html', {})
+	beamline_list = Beamline.objects.order_by('name')
+	context_dict = {'beamlines:' beamline_list}
+	return render(request, 'acisStatus/status.html', context_dict)
